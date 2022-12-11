b6ec631e9d91963818bf79ac1e6d3104633cc4a8
Th3Gavst3r <th3gavst3r@gmail.com>
2022-12-10 23:45:13 -0500

    Implement OAuth login

---
+++ src/auth/TwitterOAuth2.ts
@@ -0,0 +1,192 @@
+import OAuth2Strategy, {
+  InternalOAuthError,
+  VerifyCallback,
+} from 'passport-oauth2';
+import { auth } from 'twitter-api-sdk';
+import { findMyUser, TwitterResponse } from 'twitter-api-sdk/dist/types';
+import { getEnvironmentVariableOrThrow } from '../util/Validation';
+
+/**
+ * Creates an OAuth2 client which can access Twitter's OAuth2 API using the
+ * input credentials.
+ * @param options The configuration used to generate the OAuth2 client.
+ * @returns An OAuth2 client configured with the input credentials.
+ */
+export function getAuthClient(options: {
+  scopes: auth.OAuth2Scopes[];
+  accessToken?: string;
+  refreshToken?: string;
+}): auth.OAuth2User {
+  return new auth.OAuth2User({
+    client_id: getEnvironmentVariableOrThrow('CLIENT_ID'),
+    client_secret: getEnvironmentVariableOrThrow('CLIENT_SECRET'),
+    callback: `${getEnvironmentVariableOrThrow('BASE_URL')}/auth/callback`,
+    scopes: options.scopes,
+    token: {
+      access_token: options.accessToken,
+      refresh_token: options.refreshToken,
+    },
+  });
+}
+
+export type TwitterStrategyOptions = Omit<
+  OAuth2Strategy.StrategyOptions,
+  'authorizationURL' | 'tokenURL' | 'state'
+> & {
+  authorizationURL?: string;
+  tokenURL?: string;
+  clientType?: 'private' | 'public';
+  skipExtendedProfile?: boolean;
+  scope?: auth.OAuth2Scopes[];
+  state?: boolean;
+};
+
+/**
+ * An implementation of passport.js's generic OAuth2Strategy which integrates
+ * with Twitter's OAuth2 API.
+ */
+export class TwitterStrategy extends OAuth2Strategy {
+  public name = 'twitter';
+  public profileUrl = 'https://api.twitter.com/2/users/me';
+
+  constructor(
+    private options: TwitterStrategyOptions,
+    private verify: (
+      accessToken: string,
+      refreshToken: string,
+      profile: TwitterResponse<findMyUser>['data'],
+      verified: VerifyCallback
+    ) => void
+  ) {
+    // Private clients send requests using "Authorization" header as per docs:
+    // https://developer.twitter.com/en/docs/authentication/oauth-2-0/user-access-token
+    options.customHeaders = {
+      ...(options.clientType === 'private'
+        ? {
+            Authorization:
+              'Basic ' +
+              Buffer.from(
+                `${options.clientID}:${options.clientSecret}`
+              ).toString('base64'),
+          }
+        : {}),
+      ...(options.customHeaders || {}),
+    };
+
+    options.clientType = options.clientType || 'public';
+    options.authorizationURL =
+      options.authorizationURL || 'https://twitter.com/i/oauth2/authorize';
+    options.tokenURL =
+      options.tokenURL || 'https://api.twitter.com/2/oauth2/token';
+    options.pkce = options.pkce || true;
+    options.state = options.state || true;
+    options.scope = options.scope || [
+      'tweet.read',
+      'offline.access', // required for refresh tokens to be issued
+      'users.read',
+    ];
+    options.skipExtendedProfile = options.skipExtendedProfile || false;
+    if (
+      !options.skipExtendedProfile &&
+      options.scope.indexOf('users.read') === -1
+    ) {
+      options.skipExtendedProfile = true;
+    }
+
+    super(
+      {
+        ...options,
+        authorizationURL: options.authorizationURL,
+        tokenURL: options.tokenURL,
+      },
+      verify
+    );
+  }
+
+  /**
+   * Overrides the default profile generation with a Twitter-specific
+   * implementation.
+   * @param accessToken The access token provided by the OAuth2 callback.
+   * @param done The callback which accepts the user's profile.
+   */
+  public userProfile(
+    accessToken: string,
+    done: (
+      err?: Error | null,
+      profile?: TwitterResponse<findMyUser>['data']
+    ) => void
+  ): void {
+    this._oauth2.useAuthorizationHeaderforGET(true);
+    let url = this.profileUrl;
+    if (!this.options.skipExtendedProfile) {
+      url =
+        url +
+        '?user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld';
+    }
+
+    this._oauth2.get(
+      url,
+      accessToken,
+      function (
+        err: { statusCode: number; data?: any },
+        body?: string | Buffer
+      ): void {
+        if (err) {
+          return done(
+            new InternalOAuthError('failed to fetch user profile', err)
+          );
+        }
+
+        let profile: TwitterResponse<findMyUser>['data'];
+
+        if (body) {
+          try {
+            const parsedBody = JSON.parse(body.toString()).data;
+
+            profile = {
+              provider: 'twitter',
+              _response: parsedBody,
+              _raw: body,
+              ...parsedBody,
+            };
+          } catch (e) {
+            return done(
+              new InternalOAuthError('failed to parse profile response', e)
+            );
+          }
+        }
+
+        return done(null, profile);
+      }.bind(this)
+    );
+  }
+}
+
+/**
+ * Creates a Twitter authentication strategy tailored to this app's
+ * requirements.
+ * @returns The autoconfigured passport strategy.
+ */
+export function getTwitterStrategy(): TwitterStrategy {
+  return new TwitterStrategy(
+    {
+      clientType: 'private',
+      clientID: getEnvironmentVariableOrThrow('CLIENT_ID'),
+      clientSecret: getEnvironmentVariableOrThrow('CLIENT_SECRET'),
+      scope: ['tweet.read', 'users.read', 'like.read', 'offline.access'],
+      callbackURL: `${getEnvironmentVariableOrThrow('BASE_URL')}/auth/callback`,
+    },
+    (
+      accessToken: string,
+      refreshToken: string,
+      profile: TwitterResponse<findMyUser>['data'],
+      done: VerifyCallback
+    ) => {
+      if (!profile) {
+        return done(null, undefined);
+      }
+
+      return done(null, { id: profile.id, accessToken, refreshToken });
+    }
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,134 @@
 import { PrismaClient } from '@prisma/client';
-import { Client } from 'twitter-api-sdk';
+import { auth, Client } from 'twitter-api-sdk';
 import logger from './logger';
 import TwitterService from './service/TwitterService';
 import { getEnvironmentVariableOrThrow } from './util/Validation';
 import JobManager, { JobType } from './importer/JobManager';
 import FileImporter from './importer/FileImporter';
 import TwitterImporter from './importer/TwitterImporter';
+import express from 'express';
+import expressSession from 'express-session';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { randomUUID } from 'crypto';
+import passport from 'passport';
+import { getAuthClient, getTwitterStrategy } from './auth/TwitterOAuth2';
+import { TokenError } from 'passport-oauth2';
+
+declare global {
+  namespace Express {
+    interface User {
+      id: string;
+      accessToken: string;
+      refreshToken: string;
+    }
+  }
+}
 
 const prisma = new PrismaClient();
 
-const twitterClient = new Client(getEnvironmentVariableOrThrow('BEARER_TOKEN'));
+// const authClient = getAuthClient({ scopes: ['like.read', 'offline.access'] });
+// const twitterClient = new Client(authClient);
 
-const twitterService = new TwitterService(twitterClient);
-const fileImporter = new FileImporter(prisma);
-const twitterImporter = new TwitterImporter(prisma);
-const jobManager = new JobManager(
-  prisma,
-  twitterService,
-  fileImporter,
-  twitterImporter
+// const twitterService = new TwitterService(twitterClient);
+// const fileImporter = new FileImporter(prisma);
+// const twitterImporter = new TwitterImporter(prisma);
+// const jobManager = new JobManager(
+//   prisma,
+//   twitterService,
+//   fileImporter,
+//   twitterImporter
+// );
+
+const app = express();
+
+app.use(
+  expressSession({
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // one week
+    secret: getEnvironmentVariableOrThrow('SESSION_SECRET'),
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 1000 * 60 * 2, // 2 minutes
+    }),
+  })
 );
-
-async function main() {
-  const username = getEnvironmentVariableOrThrow('USERNAME');
-
-  const user = await twitterService.findUserByUsername(username);
-
-  // Finish incomplete jobs first
-  const deferredDownloadJobs = await jobManager.getDeferredJobs(
-    user,
-    JobType.USER_LIKES_DOWNLOAD
-  );
-  for (const job of deferredDownloadJobs) {
-    logger.info(
-      `Resuming deferred ${job.type} job for user ${job.user_id}, which was created at ${job.created_at}`
-    );
-    await jobManager.downloadUserLikesJob(job);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  const whiteList = ['/', '/login', '/auth/callback'];
+  if (whiteList.includes(req.path)) {
+    return next();
   }
 
-  // Download new likes
-  logger.info(`Downloading new likes for user ${user.id}`);
-  const freshJob = await jobManager.issueJob(user, JobType.USER_LIKES_DOWNLOAD);
-  await jobManager.downloadUserLikesJob(freshJob);
-}
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+});
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async e => {
-    logger.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+passport.serializeUser((user, done) => {
+  done(null, JSON.stringify(user));
+});
+
+passport.deserializeUser((str: string, done) => {
+  done(null, JSON.parse(str));
+});
+
+passport.use(getTwitterStrategy(prisma));
+
+app.get('/', (req, res) => res.send('Hello, world!'));
+
+app.get('/login', passport.authenticate('twitter'));
+
+app.get(
+  '/auth/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
+
+app.post('/download', (req, res) => {
+  res.sendStatus(201);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  logger.info(`Server is listening on port ${port}`);
+});
+
+// async function main() {
+//   const username = getEnvironmentVariableOrThrow('USERNAME');
+
+//   const user = await twitterService.findUserByUsername(username);
+
+//   // Finish incomplete jobs first
+//   const deferredDownloadJobs = await jobManager.getDeferredJobs(
+//     user,
+//     JobType.USER_LIKES_DOWNLOAD
+//   );
+//   for (const job of deferredDownloadJobs) {
+//     logger.info(
+//       `Resuming deferred ${job.type} job for user ${job.user_id}, which was created at ${job.created_at}`
+//     );
+//     await jobManager.downloadUserLikesJob(job);
+//   }
+
+//   // Download new likes
+//   logger.info(`Downloading new likes for user ${user.id}`);
+//   const freshJob = await jobManager.issueJob(user, JobType.USER_LIKES_DOWNLOAD);
+//   await jobManager.downloadUserLikesJob(freshJob);
+// }
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async e => {
+//     logger.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
+
+// TODO: Check what happens when file streams break (i.e. when disconnecting vpn)
+// TODO: Use OAuth for user selection
